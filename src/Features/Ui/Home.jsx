f991cd5d0a4189b3050cc240ce5389f2f221d9ab
MohamedEl-Masry <mohamed.elmasry9722@gmail.com>
2024-04-17 04:59:53 +0200

    img1

--- src/Features/Ui/Home.jsx
+++ src/Features/Ui/Home.jsx
@@ -32,18 +32,18 @@ function Home() {
       </div>
       <div className=" relative ms-2 w-3/5 place-self-center ">
         <div className="mx-auto  sm:w-full sm:pt-5">
-          <img src="./../assets/banner-img.png " alt="pizza " />
+          <img src="./../../assets/banner-img.png " alt="pizza " />
         </div>
         <div>
           <img
-            src="./../assets/home-leaf.png"
+            src="../../../assets/home-leaf.png"
             alt="home-leaf"
             className="absolute -right-1 top-0 -z-10 w-3/5 sm:-right-5 sm:pt-2 xl:w-auto"
           />
         </div>
       </div>
       <div className="absolute bottom-3 right-0  w-2/12 rotate-180 sm:w-1/5">
-        <img src="/assets/black-jamun.png" alt="black-jamun" />
+        <img src="../../assets/black-jamun.png" alt="black-jamun" />
       </div>
       <div className="absolute bottom-3 right-40 w-2/12 sm:right-5 sm:w-3/5 ">
         <img src="/assets/onion.png" alt="onion" className="m-auto" />
